76627ead1d258823055ba24dbb6c9b61c53ba7ff
Andrzej Szypulski <szypul86@gmail.com>
2019-10-11 14:00:13 +0200

    first bean used

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,28 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    private static final  String CONFIG_LOCATION = "beans.xml";
+
     public static void main(String[] args) {
         logger.info("Guess the number Game");
+
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        logger.info("number = {}", number);
+
+        context.close();
     }
 }
