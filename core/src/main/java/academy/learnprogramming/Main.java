dbf40e823d866554c670599aee2c61a6f6526818
Andrzej Szypulski <szypul86@gmail.com>
2019-10-11 14:38:33 +0200

    constructor based di

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -9,20 +9,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private static final  String CONFIG_LOCATION = "beans.xml";
+    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         logger.info("Guess the number Game");
 
+        //create context
         ConfigurableApplicationContext context
                 = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
 
+        //get number generator bean from context
         NumberGenerator numberGenerator
                 = context.getBean("numberGenerator", NumberGenerator.class);
 
+        //call method next() to get a random number
         int number = numberGenerator.next();
 
         logger.info("number = {}", number);
+
+        //get the game bean from context
+        Game game = context.getBean(Game.class);
+
+        //call reset() method
+        game.reset();
 
         context.close();
     }
