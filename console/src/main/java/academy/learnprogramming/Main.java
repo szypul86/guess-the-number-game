67b5419900f3d33b43c8c138c909c285bf4bcbba
Andrzej Szypulski <szypul86@gmail.com>
2019-11-01 19:32:00 +0100

    rearranging to Spring Boot

--- console/src/main/java/academy/learnprogramming/Main.java
+++ console/src/main/java/academy/learnprogramming/Main.java
@@ -1,23 +1,21 @@
-package academy.learnprogramming.console;
+package academy.learnprogramming;
 
 import academy.learnprogramming.config.GameConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
     //private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Guess the number Game");
-
-        //create context
-        ConfigurableApplicationContext context
-                = new AnnotationConfigApplicationContext(GameConfig.class);
-
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
