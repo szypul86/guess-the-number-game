912824d10f3ca04bda612e34da93441334bcb4e1
Andrzej Szypulski <szypul86@gmail.com>
2019-11-05 14:42:10 +0100

    learning Thymeleaf WIP

---
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming.util;
+
+public class ViewNames {
+
+    //constants
+    public static final String HOME = "home";
+    //constructors
+    private ViewNames(){};
+}
